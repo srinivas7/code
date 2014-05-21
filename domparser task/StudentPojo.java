1be34b38e0fd07fe9cc3bf36e7bfcbc06c5d26c1
srinivas <srinivas.bavirisetty@valuelabs.net>
2014-05-21 15:21:13 +0530

    dom parser task

---
+++ domparser task/StudentPojo.java
@@ -0,0 +1,42 @@
+package com.domparser;
+
+import java.util.Date;
+
+public class StudentPojo  {
+	
+	String firstname;
+	String lastname;
+	Date dob;
+	String address;
+	
+	
+	
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+		
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+
+}
